38f36f9782d7026fb384a62726c8c739df9f80b4
konainraza105 <konainraza105@gmail.com>
2025-02-28 13:07:32 +0500

    reverted

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from '@vercel/remix/vite';
 
 installGlobals({ nativeFetch: true });
 
@@ -48,6 +49,7 @@ export default defineConfig({
   },
   plugins: [
     remix({
+      presets: [vercelPreset()],
       ignoredRouteFiles: ["**/.*"],
       future: {
         v3_fetcherPersist: true,
